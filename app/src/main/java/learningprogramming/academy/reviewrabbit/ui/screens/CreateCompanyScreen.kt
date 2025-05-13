baa920e9f8241b6e814afe84fa9a8d1c091d038f
Joel <joel@Alexs-MacBook-Pro-2.local>
2025-05-13 16:51:23 +0100

    feat: create 3 different placeholder screens for the navigation items at bottom bar

---
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/ui/screens/CreateCompanyScreen.kt
@@ -0,0 +1,11 @@
+package learningprogramming.academy.reviewrabbit.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CreateCompanyScreen() {
+    Text(
+        text = "This is Create Company Page"
+    )
+}

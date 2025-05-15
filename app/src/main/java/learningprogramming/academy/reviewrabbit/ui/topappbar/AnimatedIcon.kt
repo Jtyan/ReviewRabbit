444ba17aa657c79af3b3b2327bc51a5de48e5480
Joeltt <joelttm15@gmail.com>
2025-05-15 10:52:20 +0100

    feat: modify AnimatedIcon component to be used in SearchFilter composable

--- app/src/main/java/learningprogramming/academy/reviewrabbit/ui/topappbar/AnimatedIcon.kt
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/ui/topappbar/AnimatedIcon.kt
@@ -5,28 +5,31 @@ import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
-fun AnimatedIcon(isExpanded: Boolean) {
-    val transition = updateTransition(targetState = isExpanded, label = "Icon Transition")
+fun AnimatedIcon(
+    targetState: Boolean,
+    iconIfTrue: ImageVector,
+    iconIfFalse: ImageVector,
+    contentDescription: String
+) {
+    val transition = updateTransition(targetState = targetState, label = "Icon Transition")
 
     val rotation by transition.animateFloat(
         transitionSpec = { tween(durationMillis = 300, easing = LinearOutSlowInEasing) },
         label = "Rotation Animation"
     ) { expanded ->
-        if (expanded) 90f else 0f
+        if (expanded) 0f else -180f
     }
     Icon(
-        imageVector = if (isExpanded) Icons.Filled.Clear else Icons.Filled.Menu,
-        contentDescription = "TopBar Menu",
+        imageVector = if (targetState) iconIfTrue else iconIfFalse,
+        contentDescription = contentDescription,
         modifier = Modifier.graphicsLayer {
             rotationZ = rotation
         }
--- app/src/main/java/learningprogramming/academy/reviewrabbit/ui/topappbar/TopAppBar.kt
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/ui/topappbar/TopAppBar.kt
@@ -9,6 +9,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,11 +21,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import learningprogramming.academy.reviewrabbit.R
 import learningprogramming.academy.reviewrabbit.ui.theme.ReviewRabbitTheme
@@ -58,7 +58,12 @@ fun ReviewRabbitTopAppBar(
                         .padding(horizontal = 4.dp)
                         .size(36.dp)
                 ) {
-                    AnimatedIcon(isExpanded)
+                    AnimatedIcon(
+                        isExpanded,
+                        iconIfTrue = Icons.Filled.Clear,
+                        iconIfFalse = Icons.Filled.Menu,
+                        contentDescription = "Top Menu"
+                    )
                 }
             },
             colors = TopAppBarDefaults.topAppBarColors(

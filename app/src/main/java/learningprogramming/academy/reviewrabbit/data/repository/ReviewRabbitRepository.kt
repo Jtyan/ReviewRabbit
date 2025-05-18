dd85b169259700610f142140eed2962079c64da6
Joeltt <joelttm15@gmail.com>
2025-05-18 11:58:47 +0100

    implement repository with Hilt DI and a RepositoryModule to bind Repository interface with its implementation class

---
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/data/repository/ReviewRabbitRepository.kt
@@ -0,0 +1,28 @@
+package learningprogramming.academy.reviewrabbit.data.repository
+
+import learningprogramming.academy.reviewrabbit.data.model.CompanyApiResponse
+import learningprogramming.academy.reviewrabbit.data.model.CompanyFilters
+import learningprogramming.academy.reviewrabbit.data.network.ReviewRabbitApiService
+import javax.inject.Inject
+import javax.inject.Singleton
+
+interface ReviewRabbitRepository {
+    suspend fun getAllCompanies(): List<CompanyApiResponse>
+    suspend fun getCompanyById(companyId: String): CompanyApiResponse
+    suspend fun getCompanyFilters(): CompanyFilters
+}
+
+@Singleton
+class ReviewRabbitRepositoryImpl @Inject constructor(private val apiService: ReviewRabbitApiService): ReviewRabbitRepository {
+    override suspend fun getAllCompanies(): List<CompanyApiResponse> {
+        return apiService.getAllCompanies()
+    }
+
+    override suspend fun getCompanyById(companyId: String): CompanyApiResponse {
+        return apiService.getCompanyById(companyId)
+    }
+
+    override suspend fun getCompanyFilters(): CompanyFilters {
+        return apiService.getCompanyFilters()
+    }
+}

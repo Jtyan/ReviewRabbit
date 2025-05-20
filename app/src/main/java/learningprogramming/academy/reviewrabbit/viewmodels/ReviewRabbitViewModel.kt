b3a2a24301fc979da35e138d931c39e442444d99
Joeltt <joelttm15@gmail.com>
2025-05-20 10:58:21 +0100

    feat: create a getListOfCompanies function in viewModel

--- app/src/main/java/learningprogramming/academy/reviewrabbit/viewmodels/ReviewRabbitViewModel.kt
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/viewmodels/ReviewRabbitViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import learningprogramming.academy.reviewrabbit.data.model.CompanyApiResponse
 import learningprogramming.academy.reviewrabbit.data.model.CompanyFilters
 import learningprogramming.academy.reviewrabbit.data.repository.ReviewRabbitRepository
 import javax.inject.Inject
@@ -18,16 +19,30 @@ class ReviewRabbitViewModel @Inject constructor(
 ): ViewModel() {
     private val _companyFilters = MutableStateFlow(CompanyFilters())
     val companyFilters: StateFlow<CompanyFilters> = _companyFilters.asStateFlow()
+    private val _listOfCompanies = MutableStateFlow<List<CompanyApiResponse>>(emptyList())
+    val listOfCompanies: StateFlow<List<CompanyApiResponse>> = _listOfCompanies.asStateFlow()
 
     init {
         loadCompanyFilters()
+        getListOfCompanies()
     }
+
     fun loadCompanyFilters() {
         viewModelScope.launch {
             try {
                 _companyFilters.value = reviewRabbitRepository.getCompanyFilters()
             } catch (e: Exception) {
                 Log.e("ReviewRabbitViewModel", "Error fetching company filters. $e")
+            }
+        }
+    }
+
+    fun getListOfCompanies() {
+        viewModelScope.launch {
+            try {
+                _listOfCompanies.value = reviewRabbitRepository.getAllCompanies()
+            } catch (e: Exception) {
+                Log.e("ReviewRabbitViewModel", "Error fetching list of all companies. $e")
             }
         }
     }

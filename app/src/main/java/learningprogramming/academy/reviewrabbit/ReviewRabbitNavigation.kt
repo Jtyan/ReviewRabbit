942a2642af4d173243b70be41e7b54ffba4d48c4
Joeltt <joelttm15@gmail.com>
2025-05-22 10:49:42 +0100

    feat: implement onClick function for the Reviews button and a composable route to navigate to company page

--- app/src/main/java/learningprogramming/academy/reviewrabbit/ReviewRabbitNavigation.kt
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/ReviewRabbitNavigation.kt
@@ -1,11 +1,17 @@
 package learningprogramming.academy.reviewrabbit
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import learningprogramming.academy.reviewrabbit.model.NavigationTab
+import androidx.navigation.navArgument
+import learningprogramming.academy.reviewrabbit.model.ScreenRoutes
+import learningprogramming.academy.reviewrabbit.ui.screens.CompanyPage
 import learningprogramming.academy.reviewrabbit.ui.screens.CreateCompanyScreen
 import learningprogramming.academy.reviewrabbit.ui.screens.HomeScreen
 import learningprogramming.academy.reviewrabbit.ui.screens.UserSettingsScreen
@@ -17,15 +23,44 @@ fun ReviewRabbitApp(
 ) {
     val viewModel: ReviewRabbitViewModel = hiltViewModel()
 
-    NavHost(navController = navController, startDestination = NavigationTab.HOME.label) {
-        composable(route = NavigationTab.HOME.label) {
-            HomeScreen(viewModel)
+    NavHost(navController = navController, startDestination = ScreenRoutes.HOME) {
+        composable(route = ScreenRoutes.HOME) {
+            HomeScreen(
+                viewModel,
+                onClick = { companyId ->
+                    navController.navigate(ScreenRoutes.companyPageWithArg(companyId))
+                }
+            )
         }
-        composable(route = NavigationTab.CREATE_NEW_COMPANY.label) {
+        composable(route = ScreenRoutes.CREATE_NEW_COMPANY) {
             CreateCompanyScreen()
         }
-        composable(route = NavigationTab.USER_SETTINGS.label){
+        composable(route = ScreenRoutes.USER_SETTINGS) {
             UserSettingsScreen()
+        }
+        composable(
+            route = ScreenRoutes.COMPANY_PAGE,
+            arguments = listOf(navArgument("companyId") { type = NavType.IntType }),
+            enterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { fullHeight -> fullHeight },
+                    animationSpec = tween(durationMillis = 500, delayMillis = 0)
+                )
+            },
+            exitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { fullHeight -> fullHeight },
+                    animationSpec = tween(durationMillis = 500)
+                )
+            },
+        ) { backStackEntry ->
+            val companyId = backStackEntry.arguments?.getInt("companyId")
+            if (companyId != null) {
+                CompanyPage(
+                    viewModel,
+                    companyId = companyId
+                )
+            }
         }
     }
 }

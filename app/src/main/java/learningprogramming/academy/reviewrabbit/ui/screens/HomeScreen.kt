c9a02a98989784df4c66fb6a05bfe7286e0b9eff
Joeltt <joelttm15@gmail.com>
2025-05-27 15:27:31 +0100

    fix: add SearchFilter as part of the lazyColumn to enable scrolling in landscape orientation

--- app/src/main/java/learningprogramming/academy/reviewrabbit/ui/screens/HomeScreen.kt
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/ui/screens/HomeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -63,7 +64,10 @@ fun HomeScreen(
     }
     val isBannerVisible = !isScrolled
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         AnimatedVisibility(
             visible = isBannerVisible,
             enter = fadeIn() + expandVertically(expandFrom = Alignment.Top),
@@ -74,14 +78,14 @@ fun HomeScreen(
         ) {
             HomepageHeroBanner()
         }
-
-        SearchFilters(homeScreenViewModel)
-
         LazyColumn(
             state = lazyListState,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth()
         ) {
+            item {
+                SearchFilters(homeScreenViewModel)
+            }
             items(listOfCompanies) {
                 LazyColumnCompanyItem(
                     it,

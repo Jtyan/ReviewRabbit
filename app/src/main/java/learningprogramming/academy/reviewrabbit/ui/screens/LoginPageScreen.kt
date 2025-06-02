ae573cd02d714c4375bafc18b371acd9ecde6df6
Joeltt <joelttm15@gmail.com>
2025-06-02 16:54:11 +0100

    feat: pass in userViewModel to LoginPageScreen and implement different LoginUiState handlings

--- app/src/main/java/learningprogramming/academy/reviewrabbit/ui/screens/LoginPageScreen.kt
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/ui/screens/LoginPageScreen.kt
@@ -4,45 +4,70 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
-
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import learningprogramming.academy.reviewrabbit.data.model.PostUserLoginApi
 import learningprogramming.academy.reviewrabbit.ui.components.common.CustomButton
 import learningprogramming.academy.reviewrabbit.ui.components.common.CustomTextField
-import learningprogramming.academy.reviewrabbit.ui.theme.ReviewRabbitTheme
 import learningprogramming.academy.reviewrabbit.ui.theme.extendedLight
 import learningprogramming.academy.reviewrabbit.util.PasswordOutputTransformation
+import learningprogramming.academy.reviewrabbit.viewmodels.LoginScreenUiState
+import learningprogramming.academy.reviewrabbit.viewmodels.UserViewModel
 
 @Composable
 fun LoginPageScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    userViewModel: UserViewModel,
+    onLoginSuccessNavigation: () -> Unit
 ) {
     var isPasswordVisible by rememberSaveable { mutableStateOf(false) }
+
+    val emailState = rememberTextFieldState()
+    val passwordState = rememberTextFieldState()
+
+    val loginUiState by userViewModel.loginUiState.collectAsState()
+
+    DisposableEffect(Unit) {
+        onDispose {
+            userViewModel.resetLoginStateToIdle()
+        }
+    }
+
+    LaunchedEffect(loginUiState) {
+        if (loginUiState is LoginScreenUiState.Success) {
+            onLoginSuccessNavigation()
+            userViewModel.resetLoginStateToIdle()
+        }
+    }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -56,27 +81,28 @@ fun LoginPageScreen(
             fontSize = 24.sp,
             color = MaterialTheme.colorScheme.secondaryContainer
         )
-        Box(
-            modifier = Modifier
-                .padding(12.dp)
-                .border(
-                    width = 1.dp,
-                    color = Color.Red
+        Spacer(modifier = Modifier.height(24.dp))
+        if (loginUiState is LoginScreenUiState.Error) {
+            Box(
+                modifier = Modifier
+                    .border(width = 1.dp, color = MaterialTheme.colorScheme.error)
+                    .padding(12.dp)
+            ) {
+                Text(
+                    text = (loginUiState as LoginScreenUiState.Error).message,
+                    fontSize = 12.sp,
+                    color = MaterialTheme.colorScheme.error,
                 )
-        ) {
-            Text(
-                text = "User email or password is invalid.",
-                color = Color.Red,
-                modifier = Modifier.padding(12.dp)
-            )
+            }
         }
         CustomTextField(
+            textFieldState = emailState,
             text = "Email",
             placeholder = "Your Email",
             required = true
         )
         CustomTextField(
-            textFieldState = rememberTextFieldState(),
+            textFieldState = passwordState,
             text = "Password",
             placeholder = "Your Password",
             trailingIcon = {
@@ -95,31 +121,34 @@ fun LoginPageScreen(
             outputTransformation = if (isPasswordVisible) null else PasswordOutputTransformation(),
             required = true
         )
-        CustomButton(
-            text = "Log In",
-            onClick = {},
-            modifier = Modifier
-                .align(Alignment.Start)
-                .width(224.dp)
-                .padding(16.dp)
-        )
-        Text(
-            text = "Forgot Password?",
-            fontSize = 14.sp,
-            textDecoration = TextDecoration.Underline,
-            color = extendedLight.forgotPassword.colorContainer,
-            modifier = Modifier
-                .align(Alignment.Start)
-                .padding(horizontal = 16.dp)
-                .clickable {}
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun LoginPageScreenPreview() {
-    ReviewRabbitTheme(dynamicColor = false) {
-        LoginPageScreen()
+        if (loginUiState is LoginScreenUiState.Loading) {
+            CircularProgressIndicator()
+        } else {
+            CustomButton(
+                text = "Log In",
+                onClick = {
+                    userViewModel.onLoginClicked(
+                        PostUserLoginApi(
+                            email = emailState.text.toString(),
+                            password = passwordState.text.toString()
+                        )
+                    )
+                },
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .width(224.dp)
+                    .padding(16.dp)
+            )
+            Text(
+                text = "Forgot Password?",
+                fontSize = 14.sp,
+                textDecoration = TextDecoration.Underline,
+                color = extendedLight.forgotPassword.colorContainer,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(horizontal = 16.dp)
+                    .clickable {}
+            )
+        }
     }
 }

3708097675b6aff2cb972d90273b75ada87695ab
Joeltt <joelttm15@gmail.com>
2025-06-18 15:31:32 +0100

    chore: merge ReviewApiReponse, ReviewSummaryApiResponse, PostReviewApi into one data model file and rename CompanyApiResponse to CompanyApiModel, InvitesApiResponse to InvitesApiModels

--- app/src/main/java/learningprogramming/academy/reviewrabbit/data/model/ReviewApiModels.kt
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/data/model/ReviewApiModels.kt
@@ -3,6 +3,17 @@ package learningprogramming.academy.reviewrabbit.data.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class PostReviewApi(
+    val companyId: Int,
+    val management: Int,
+    val culture: Int,
+    val salary: Int,
+    val benefits: Int,
+    val wouldRecommend: Int,
+    val review: String,
+)
+
+@Serializable
 data class ReviewApiResponse(
     val id: Long,
     val companyId: Long,
@@ -15,4 +26,11 @@ data class ReviewApiResponse(
     val review: String,
     val created: String,
     val updated: String
+)
+
+@Serializable
+data class ReviewSummaryApiResponse(
+    val companyId: Int,
+    val contents: String,
+    val created: String
 )

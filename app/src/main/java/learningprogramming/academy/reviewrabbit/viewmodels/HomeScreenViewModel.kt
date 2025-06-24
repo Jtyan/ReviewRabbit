10de76dea61309bff90918558efd69497fe1935d
Joeltt <joelttm15@gmail.com>
2025-06-24 12:58:38 +0100

    feat: create getListOfFilteredCompanies and toggleFilter in HomeScreenViewModel to get list of filtered companies and add/remove filter in the list of filters respectively

--- app/src/main/java/learningprogramming/academy/reviewrabbit/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/learningprogramming/academy/reviewrabbit/viewmodels/HomeScreenViewModel.kt
@@ -10,35 +10,40 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import learningprogramming.academy.reviewrabbit.data.model.CompanyApiResponse
 import learningprogramming.academy.reviewrabbit.data.model.CompanyFilters
+import learningprogramming.academy.reviewrabbit.data.repository.CompanyApiResult
 import learningprogramming.academy.reviewrabbit.data.repository.CompanyRepository
+import learningprogramming.academy.reviewrabbit.model.FilterTabs
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeScreenViewModel @Inject constructor(
     private val companyRepository: CompanyRepository
-): ViewModel() {
+) : ViewModel() {
     private val _companyFilters = MutableStateFlow(CompanyFilters())
     val companyFilters: StateFlow<CompanyFilters> = _companyFilters.asStateFlow()
     private val _listOfCompanies = MutableStateFlow<List<CompanyApiResponse>>(emptyList())
     val listOfCompanies: StateFlow<List<CompanyApiResponse>> = _listOfCompanies.asStateFlow()
+    private val _listOfFilters = MutableStateFlow(CompanyFilters())
+    val listOfFilters: StateFlow<CompanyFilters> = _listOfFilters.asStateFlow()
 
 
     init {
         loadCompanyFilters()
-        getListOfCompanies()
+        getListOfEveryCompanies()
     }
 
     private fun loadCompanyFilters() {
         viewModelScope.launch {
             try {
                 _companyFilters.value = companyRepository.getCompanyFilters()
+                Log.i("HomeScreenViewModel", _companyFilters.value.toString())
             } catch (e: Exception) {
                 Log.e("HomeScreenViewModel", "Error fetching company filters. $e")
             }
         }
     }
 
-    private fun getListOfCompanies() {
+    private fun getListOfEveryCompanies() {
         viewModelScope.launch {
             try {
                 _listOfCompanies.value = companyRepository.getAllCompanies()
@@ -46,5 +51,54 @@ class HomeScreenViewModel @Inject constructor(
                 Log.e("HomeScreenViewModel", "Error fetching list of all companies. $e")
             }
         }
+    }
+
+    fun getListOfFilteredCompanies(companyFilters: CompanyFilters) {
+        viewModelScope.launch {
+            try {
+                Log.i("HomeScreenViewModel", companyFilters.toString())
+                val response = companyRepository.getFilteredCompaniesList(companyFilters)
+                if (response is CompanyApiResult.GetFilteredCompaniesSuccess) {
+                    _listOfCompanies.value = response.filteredCompanies
+                    Log.i("HomeScreenViewModel", "list of companies : ${listOfCompanies.value}")
+                }
+            } catch (e: Exception) {
+                Log.e("HomeScreenViewModel", "Error fetching list of filtered companies. $e")
+            }
+        }
+    }
+
+    fun toggleFilter(item: String, category: FilterTabs) {
+        val currentFilters = _listOfFilters.value
+
+        val newFilters = when (category) {
+            FilterTabs.LOCATIONS -> {
+                val newList = currentFilters.locations.toMutableList().apply {
+                    if (contains(item)) remove(item) else add(item)
+                }
+                currentFilters.copy(locations = newList)
+            }
+            FilterTabs.INDUSTRIES -> {
+                val newList = currentFilters.industries.toMutableList().apply {
+                    if (contains(item)) remove(item) else add(item)
+                }
+                currentFilters.copy(industries = newList)
+            }
+
+            FilterTabs.COUNTRIES -> {
+                val newList = currentFilters.countries.toMutableList().apply {
+                    if (contains(item)) remove(item) else add(item)
+                }
+                currentFilters.copy(countries = newList)
+            }
+
+            FilterTabs.TAGS -> {
+                val newList = currentFilters.tags.toMutableList().apply {
+                    if (contains(item)) remove(item) else add(item)
+                }
+                currentFilters.copy(tags = newList)
+            }
+        }
+        _listOfFilters.value = newFilters
     }
 }
